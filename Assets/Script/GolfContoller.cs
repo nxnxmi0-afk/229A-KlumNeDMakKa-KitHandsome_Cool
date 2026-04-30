78dd00cbdbda3f8a999f6e5a582f62df0dc50a51
KITTIPOD0 <kittipod.sinr@bumail.net>
2026-05-01 00:52:48 +0700

    set_strick

--- Assets/Script/GolfContoller.cs
+++ Assets/Script/GolfContoller.cs
@@ -3,13 +3,8 @@ using UnityEngine.UI;
 
 public class GolfContoller : MonoBehaviour
 {
-    float hitForce, SliderHitForce;
-    Vector3 lastPositionsenser;
-    Slider forceslider;
-        void OnSliderValueChange()
-    {
-        SliderHitForce = hitForce * forceslider.value;
-    }
+
+       
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/Script/StirckController.cs
+++ Assets/Script/StirckController.cs
@@ -1,13 +1,23 @@
 using Unity.Multiplayer.PlayMode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StirckController : MonoBehaviour
 {
     private Transform Stick;
     public Camera maincamera;
 
-    Vector3 lastPositionsenser;
-    void handleMouseInput()
+    float hitforce;
+    float Sliderhitforce;
+
+    
+    Slider forceSlider;
+
+    public void OnSliderValueChange()
+    {
+        Sliderhitforce = hitforce * forceSlider.value; 
+    }
+       void handleMouseInput()
     {
         Vector3 mousePos = maincamera.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = Stick.position.z;
@@ -17,38 +27,8 @@ public class StirckController : MonoBehaviour
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
         Stick.rotation = Quaternion.Euler(0f, 0f, angle);
-/*
-        if (Input.GetMouseButtonDown(0))
-        {
-            lastPositionsenser = GetMouseWorldPosition();
-        }
-        if(Input.GetMouseButton(0))
-        {
-            Vector3 currentMousePosition = GetMouseWorldPosition();
-
-            Vector3 lastDirection = lastPositionsenser - Stick.position;
-            Vector3 currentDirection = currentMousePosition - Stick.position;
-
-            float angle = Vector3.SignedAngle(lastDirection, currentDirection, Vector3.forward);
-
-            Stick.Rotate(Vector3.forward,angle * 2,Space.World);
-
-            lastPositionsenser = currentMousePosition;
-        }*/
-
     }
-  /*  Vector3 GetMouseWorldPosition()
-    {
-        Ray ray = maincamera.ScreenPointToRay(Input.mousePosition);
-        Plane plane = new Plane(Vector3.up,Stick.position);
-
-        if (plane.Raycast(ray,out float distance))
-        {
-            return ray.GetPoint(distance);
-        }
-        return Vector3.zero;
-    }*/
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+       
     void Start()
     {
         Stick = GetComponent<Transform>();
