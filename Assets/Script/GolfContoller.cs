90f51ce551a72c8c6ca71e64cbf1703077fc0d46
KITTIPOD0 <kittipod.sinr@bumail.net>
2026-05-01 20:27:51 +0700

    Hit_ball

--- Assets/Script/GolfContoller.cs
+++ Assets/Script/GolfContoller.cs
@@ -3,12 +3,13 @@ using UnityEngine.UI;
 
 public class GolfContoller : MonoBehaviour
 {
-
+    Rigidbody2D rb;
        
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
     }
 
     // Update is called once per frame
--- Assets/Script/StirckController.cs
+++ Assets/Script/StirckController.cs
@@ -5,17 +5,22 @@ using UnityEngine.UI;
 
 public class StirckController : MonoBehaviour
 {
-    
+    public Rigidbody2D ballRb;
     public Camera maincamera;
+    SpriteRenderer sr;
+    float Sliderhitforce, StickhitSpeed;
 
-    float Sliderhitforce,StickhitSpeed;
-
-    [NonSerialized]public Transform stick;
+    [NonSerialized] public Transform stick;
     Rigidbody2D rb;
 
     float pullAmount = 0f;
     float maxPull = 10f;
     Vector3 startPos;
+    [NonSerialized]public GameObject otherStick1;
+    [NonSerialized] public GameObject otherStick2;
+    [NonSerialized] public GameObject otherStick3;
+    [NonSerialized] public GameObject otherStick4;
+    bool isSelectingStick = false;
 
 
 
@@ -30,28 +35,43 @@ public class StirckController : MonoBehaviour
             Vector3 dir = mousePos - stick.position;
 
             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-      
-        
+
+
             stick.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
         }
     }
     void change()
     {
-        {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                pullAmount += Time.deltaTime * 6f;
-                pullAmount = Mathf.Clamp(pullAmount, 0, maxPull);
 
-                // ง้างจากตำแหน่งเดิม
-                stick.localPosition = startPos - stick.up * pullAmount;
-            }
-            else
-            {
-                // กลับไปตำแหน่งเดิม
-                stick.localPosition = Vector3.Lerp(stick.localPosition, startPos, Time.deltaTime * 10f);
-            }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            pullAmount += Time.deltaTime * 10f;
+            pullAmount = Mathf.Clamp(pullAmount, 0, maxPull);
+
+            // ง้างจากตำแหน่งเดิม
+            stick.localPosition = startPos - stick.up * pullAmount;
         }
+        else
+        {
+            // กลับไปตำแหน่งเดิม
+            stick.localPosition = Vector3.Lerp(stick.localPosition, startPos, Time.deltaTime * 10f);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Vector2 dir = stick.up;
+
+            float power = pullAmount / maxPull;
+            float force = power * 15f;
+
+            ballRb.AddForce(dir * force, ForceMode2D.Impulse);
+
+            pullAmount = 0f;
+            moving();
+        }
+
+
+
     }
 
     void Start()
@@ -60,13 +80,94 @@ public class StirckController : MonoBehaviour
         stick = GetComponent<Transform>();
         rb.gravityScale = 0;
         startPos = stick.localPosition;
+        sr = GetComponent<SpriteRenderer>();
+        otherStick1 = GameObject.Find("Stick1");
+        otherStick2 = GameObject.Find("Stick2");
+        otherStick3 = GameObject.Find("Stick3");
+        otherStick4 = GameObject.Find("Stick4");
+        sr.sortingOrder = 0;
+
+    }
+    void SelectStick(GameObject selected)
+    {
+        sr.sortingOrder = 1;
+        if (ballRb.velocity.magnitude >= 1f) return;
+
+        isSelectingStick = true;
+
+        otherStick1.SetActive(false);
+        otherStick2.SetActive(false);
+        otherStick3.SetActive(false);
+        otherStick4.SetActive(false);
+
+            selected.SetActive(true);
+
+         if (ballRb.velocity.sqrMagnitude > 0.05f)
+        {
+            sr.sortingOrder = 0;
+        }
+        else
+        {
+            sr.sortingOrder = 1;
+        }
+
+        
+    }
+    void moving()
+    {
+        if (ballRb.velocity.sqrMagnitude > 0.05f)
+        {
+            sr.sortingOrder = 0;
+        }
+        else
+        {
+            sr.sortingOrder = 1;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        handleMouseInput();
-        change();
-/*        stick.position = ball.position;
-*/    }
+        
+
+
+        if (!isSelectingStick)
+        {
+            handleMouseInput();
+            change();
+            
+        }
+       
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SelectStick(otherStick1);
+            isSelectingStick = false;
+            moving();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            SelectStick(otherStick2);
+            isSelectingStick = false;
+            moving();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SelectStick(otherStick3);
+            isSelectingStick = false;
+            moving();
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            SelectStick(otherStick4);
+            isSelectingStick = false;
+            moving();
+        }
+
+
+
+    }
 }
