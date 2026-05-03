1b65e9fe15d9bb43af0961fae0cb557b17638948
nxnxmi0 <nxnxmi0@gmail.com>
2026-05-03 15:41:37 +0700

    addWAter+script+colider

--- Assets/Script/GolfContoller.cs
+++ Assets/Script/GolfContoller.cs
@@ -4,17 +4,22 @@ using UnityEngine.UI;
 public class GolfContoller : MonoBehaviour
 {
     Rigidbody2D rb;
-       
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Vector2 startPosition;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0;
+        startPosition = transform.position; 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Water"))
+        {
+            transform.position = startPosition;
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+        }
     }
 }
